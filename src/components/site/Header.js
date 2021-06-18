99b0da3cc1018a7f72cece5609a3c71c9b1af46f
Maria Bruner <mariab2898@gmail.com>
2021-06-17 21:25:32 -0400

    first commit

---
+++ src/components/site/Header.js
@@ -0,0 +1,20 @@
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap'
+
+const Header = () => {
+    return(
+    <header>
+        <Navbar className='header'>
+        <Nav className = 'ml-auto' navbar>
+            <NavItem>
+            </NavItem>
+        </Nav>
+        </Navbar>
+    </header>
+    )
+}
